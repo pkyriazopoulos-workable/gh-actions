53edbdb8da970cb52414923391ed8919e3d50514
Panagiotis Kyriazopoulos <panagiotis.kyriazopoulos@workable.com>
2023-05-05 18:33:58 +0300

    Setup monorepo

---
+++ apps/foo/src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+    return <h1 id="title">Foo</h1>; 
+}
+
+export default App;
---
+++ apps/foo/webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.jsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: "babel-loader",
+        include: path.join(__dirname, "src"),
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+      filename: "index.html",
+    }),
+  ],
+  devServer: {
+    port: 3000,
+  },
+};
